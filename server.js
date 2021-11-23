630b48049e3e9ff359bb138518fc615379d21d0c
AnujPundir29 <pundiranuj28@gmail.com>
2021-11-24 00:41:56 +0530

    ecommerce api

--- server.js
+++ server.js
@@ -6,16 +6,55 @@ const {
     graphqlHTTP
 } = require("express-graphql");
 
+// '!' for mandatory
 const schema = buildSchema(`
     type Query {
-        description: String
-        price: Float
+        products: [Product]
+        orders: [Order]
+    }
+    type Product { 
+        id: ID!
+        description: String!   
+        reviews: [Review]
+        price: Float!
+    }
+    type Review {
+        comment: String
+        rating: Int!
+    }
+    type Order{
+        date: String!
+        subtotal: Float! 
+        items: [OrderItem]
+    }
+    type OrderItem{
+        product: Product!
+        quantity: Int!
     }
 `);
 
 const root = {
-    description: () => "Red Shoes",
-    price: () => 100.00
+    products: [{
+        id: 'RedShoe',
+        description: 'Red Shoe',
+        reviews: [{
+            comment: 'Nice Shoes',
+            rating: 4
+        }],
+        price: 1300.44
+    }],
+    orders: [{
+        date: '24-11-2021',
+        subtotal: 1300.44,
+        items: [{
+            product: {
+                id: 'RedShoe',
+                description: 'Red Shoe',
+                price: 1300.44
+            },
+            quantity: 1
+        }]
+    }]
 };
 
 const app = express();
